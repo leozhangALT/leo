c553dd222faa4d8344c04c816477555c2d508054
TerryHintz <cza82@sfu.ca>
2020-05-24 12:52:35 -0700

    added gifs, updated css

--- src/home/Projects.js
+++ src/home/Projects.js
@@ -2,25 +2,28 @@ import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
 import './Home.css';
 
+import sortGif from './gifs/sort.gif';
+import queueGif from './gifs/queue.gif';
+
 import Button from '@material-ui/core/Button';
 
 const projects = [
     {
         name: 'Sorting Visualizer',
         desc: 'Want to see how popular sorting algorithms actually compare and modify the array? Try the Sorting Visualizer to see it in actions.',
-        image: 'https://i.imgur.com/jFXPgvL.png',
+        image: sortGif,
         route: 'sorting',
     },
     {
         name: 'Customer Booking',
         desc: 'Your walk in barber shop is doing better than ever, and the customers just keep pilling in. Try this self queueing tool to keep things organized.',
-        image: 'https://i.imgur.com/jFXPgvL.png',
+        image: sortGif,
         route: 'booking',
     },
     {
         name: 'Barber Shop Queue',
         desc: 'Your walk in barber shop is doing better than ever, and the customers just keep pilling in. Try this self queueing tool to keep things organized.',
-        image: 'https://i.imgur.com/jFXPgvL.png',
+        image: queueGif,
         route: 'queue',
     },
 ]
