c9792882ce49100f7e9d7a6a13015ae461bbb957
TerryHintz <cza82@sfu.ca>
2020-05-24 13:15:30 -0700

    update desc

--- src/home/Projects.js
+++ src/home/Projects.js
@@ -10,19 +10,22 @@ import Button from '@material-ui/core/Button';
 const projects = [
     {
         name: 'Sorting Visualizer',
-        desc: 'Want to see how popular sorting algorithms actually compare and modify the array? Try the Sorting Visualizer to see it in actions.',
+        desc1: 'Want to see how popular sorting algorithms actually compare and modify the array? The sorting visualizer highlights the values being compared and changed for your viewing pleasure.',
+        desc2: 'Randomize an array of up to 1000 integers, pick the sorting algorithm and speed of your choice, sit back and watch the magic happen.',
         image: sortGif,
         route: 'sorting',
     },
     {
         name: 'Customer Booking',
-        desc: 'Your walk in barber shop is doing better than ever, and the customers just keep pilling in. Try this self queueing tool to keep things organized.',
+        desc1: 'Your walk in barber shop is doing better than ever, and the customers just keep pilling in. Try this self queueing tool to keep things organized.',
+        desc2: '',
         image: sortGif,
         route: 'booking',
     },
     {
         name: 'Barber Shop Queue',
-        desc: 'Your walk in barber shop is doing better than ever, and the customers just keep pilling in. Try this self queueing tool to keep things organized.',
+        desc1: 'Business at your walk in barber shop is doing better than ever, your having a hard time staying organized with all customers piling in. You want to keep costs low and avoid hiring a secretary, what do you do?',
+        desc2: 'You use a simple and intuitive app for customers to queue themselves as they walk in. No need to memroize the order they walked in or using a messy system like take a number.',
         image: queueGif,
         route: 'queue',
     },
@@ -33,14 +36,15 @@ class Projects extends Component {
   render() {
     return (
       <div className='projects-root'>
-        <div style={{color: this.props.theme}} className='projects-header'>My Personal Projects</div>
+        <div style={{color: this.props.theme}} className='projects-header'>Personal Projects</div>
         {projects.map((proj, index)=>{
             return(
                 <div key={proj.name} style={{flexDirection: index%2 ? 'row-reverse' : null}} className='project-container'>
                     <img src={proj.image} className={'projects-img'} />
                     <div className={'project-right'}>
                         <p className='projects-title'>{proj.name}</p>
-                        <p className='projects-desc'>{proj.desc}</p>
+                        <p className='projects-desc'>{proj.desc1}</p>
+                        <p className='projects-desc'>{proj.desc2}</p>
                         <Button className='project-btn' style={{backgroundColor: this.props.theme}} variant="contained" color="primary">Try Now</Button>
                     </div>
                 </div>
