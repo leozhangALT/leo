f806889c88ba23b72d2f69bed154ffe3cd482645
TerryHintz <cza82@sfu.ca>
2020-05-24 14:11:41 -0700

    update paragraphs

--- src/home/Projects.js
+++ src/home/Projects.js
@@ -11,7 +11,7 @@ import Button from '@material-ui/core/Button';
 const projects = [
     {
         name: 'Sorting Visualizer',
-        desc1: 'Want to see how popular sorting algorithms actually compare and modify the array? The sorting visualizer highlights the values being compared and changed for your viewing pleasure.',
+        desc1: 'Want to see how popular sorting algorithms actually compare and modify the elements? The sorting visualizer highlights the values being compared and changed for your viewing pleasure.',
         desc2: 'Randomize an array of up to 1000 integers, pick the sorting algorithm and speed of your choice, sit back and watch the magic happen.',
         image: sortGif,
         route: 'sorting',
@@ -20,15 +20,15 @@ const projects = [
     {
         name: 'Barber Shop Queue',
         desc1: 'Business at your walk in barber shop is doing better than ever, you\'re having a hard time staying organized with all the customers piling in. You want to keep costs low and avoid hiring a secretary, what do you do?',
-        desc2: 'You use a simple and intuitive app for customers to queue themselves as they walk in. No need to memorize the order they walked in or using a messy system like take a number.',
+        desc2: 'You use a simple and intuitive app for customers to queue themselves as they walk in. No need to memorize who walked in first or use a messy system like take a number.',
         image: queueGif,
         route: 'queue',
         done: true,
     },
     {
         name: 'Customer Booking',
-        desc1: 'Business at your walk in barber shop is doing even better than before, customers wait times are increasing drastically. You don\'t customers to get impatient and walk out the door, what do you do?',
-        desc2: 'You use a beautiful online booking platform. Allowing your customers to easily book their appointments online. As customers are willing to pay more to avoid the wait. Resulting in more money in your pocket for doing the same amount of work.',
+        desc1: 'Business at your walk in barber shop is doing even better than before, but cusotmer wait times are skyrocketing. You don\'t want customers to get impatient and walk out the door, what do you do?',
+        desc2: 'You use a beautiful online booking platform, allowing your customers to easily book their appointments. Customers are willing to pay more to avoid the wait. Resulting in more money in your pocket for doing the same amount of work.',
         image: bookingGif,
         route: 'booking',
         done: false,
@@ -40,7 +40,7 @@ class Projects extends Component {
   render() {
     return (
       <div className='projects-root'>
-        {/* <div style={{color: this.props.theme}} className='projects-header'>Personal Projects</div> */}
+        <div style={{color: this.props.theme}} className='projects-header'>Projects</div>
         {projects.map((proj, index)=>{
             return(
                 <div key={proj.name} style={{flexDirection: index%2 ? 'row-reverse' : null}} className='project-container'>
