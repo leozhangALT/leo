47f9b4f36f57aebb1381b0d02f53e6654697ca5f
TerryHintz <cza82@sfu.ca>
2020-05-23 23:17:09 -0700

    setup react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, {Component} from 'react';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Home from './home/Home'
+import Temp from './sort/temp'
+import Unknown from './unknown'
 
 class App extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route path='/' exact component={Home}/>
+          <Route path='/sorting' exact component={Temp}/>
+          <Route path='/' component={Unknown}/>
+        </Switch>
+      </BrowserRouter>
     );
   }
   
--- src/home/Home.js
+++ src/home/Home.js
@@ -1,11 +1,16 @@
 import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
 import './Home.css';
 
 class Home extends Component {
+
   render() {
     return (
       <div>
-        
+        Home
+        <Link to="/sort">
+            go to sort
+        </Link>
       </div>
     );
   }
