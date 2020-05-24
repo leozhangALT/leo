1f16c0872a41bcff8a8b92aeb1efeac4999c7b24
TerryHintz <cza82@sfu.ca>
2020-05-24 13:42:18 -0700

    added customer booking

--- src/home/Projects.js
+++ src/home/Projects.js
@@ -4,6 +4,7 @@ import './Home.css';
 
 import sortGif from './gifs/sort.gif';
 import queueGif from './gifs/queue.gif';
+import bookingGif from './gifs/booking.gif';
 
 import Button from '@material-ui/core/Button';
 
@@ -14,13 +15,7 @@ const projects = [
         desc2: 'Randomize an array of up to 1000 integers, pick the sorting algorithm and speed of your choice, sit back and watch the magic happen.',
         image: sortGif,
         route: 'sorting',
-    },
-    {
-        name: 'Customer Booking',
-        desc1: 'Your walk in barber shop is doing better than ever, and the customers just keep pilling in. Try this self queueing tool to keep things organized.',
-        desc2: '',
-        image: sortGif,
-        route: 'booking',
+        done: true,
     },
     {
         name: 'Barber Shop Queue',
@@ -28,6 +23,15 @@ const projects = [
         desc2: 'You use a simple and intuitive app for customers to queue themselves as they walk in. No need to memroize the order they walked in or using a messy system like take a number.',
         image: queueGif,
         route: 'queue',
+        done: true,
+    },
+    {
+        name: 'Customer Booking',
+        desc1: 'Business at your walk in barber shop is doing even better than before, customers wait times are increasing drastically. You don\'t customers to get impatient and walk out the door, what do you do?',
+        desc2: 'You use a beautiful online booking platform. Allowing your customers to book their appointments online. As customers are willing to pay more to avoid the wait. Resulting in more money in your pocket for doing the same amount of work.',
+        image: bookingGif,
+        route: 'booking',
+        done: false,
     },
 ]
 
@@ -45,7 +49,7 @@ class Projects extends Component {
                         <p className='projects-title'>{proj.name}</p>
                         <p className='projects-desc'>{proj.desc1}</p>
                         <p className='projects-desc'>{proj.desc2}</p>
-                        <Button className='project-btn' style={{backgroundColor: this.props.theme}} variant="contained" color="primary">Try Now</Button>
+                        <Button className='project-btn' style={{backgroundColor: this.props.theme}} variant="contained" color="primary">{proj.done ? 'Try Now' : 'Work in Progress'}</Button>
                     </div>
                 </div>
             )
