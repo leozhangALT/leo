443685c319699e05e23d5a6d680806145406aa89
TerryHintz <cza82@sfu.ca>
2020-05-24 23:42:44 -0700

    copy email snackbar

--- src/home/Header.js
+++ src/home/Header.js
@@ -4,18 +4,24 @@ import Dialog from '@material-ui/core/Dialog';
 import EmailIcon from '@material-ui/icons/Email';
 import FileCopyIcon from '@material-ui/icons/FileCopy';
 import {CopyToClipboard} from 'react-copy-to-clipboard';
+import Snackbar from '@material-ui/core/Snackbar';
 
 class Header extends Component {
     state = {
-        dialog: true,
+        dialog: false,
+        copy: false,
     }
 
     toggleDialog = () => {
-        this.setState({dialog: !this.state.dialog})
+        this.setState({dialog: !this.state.dialog});
     }
 
-    copyToClip = () => {
-        
+    closeSnack = () => {
+        this.setState({copy: false});
+    }
+
+    copied = () => {
+        this.setState({copy: true});
     }
 
     render() {
@@ -28,12 +34,18 @@ class Header extends Component {
                             <EmailIcon style={{fontSize: '30px'}}/>
                             <span id='email' className='header-email'>cza82@sfu.ca</span>
                             <CopyToClipboard text={'cza82@sfu.ca'}>
-                                <FileCopyIcon style={{fontSize: '30px', cursor: 'pointer'}}/>
+                                <FileCopyIcon onClick={() => this.copied()} style={{fontSize: '30px', cursor: 'pointer'}}/>
                             </CopyToClipboard>
                         </div>
                         <p>Thank you, have a good day.</p>
                     </div>
                 </Dialog>
+                <Snackbar
+                    open={this.state.copy}
+                    autoHideDuration={3000}
+                    onClose={this.closeSnack}
+                    message='Copied to Clipboard'
+                />
                 <div className='header-container'>
                     <span onClick={() => this.toggleDialog()} className='header-contact'>Contact Me</span>
                 </div>
