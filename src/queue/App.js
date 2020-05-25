2a1cb9ea2f65899db61052351274c61430ae5e13
TerryHintz <cza82@sfu.ca>
2020-05-25 14:50:59 -0700

    github logo for queue

--- src/queue/App.js
+++ src/queue/App.js
@@ -174,7 +174,11 @@ class App extends Component {
                         </div>
                     </div>
                 </div>
-                <div className={'queue-demo-pass'}>Password for Demo: lakers23</div>
+                <div className='footer'>
+                    <img alt='github' className='github-icon' src={require('../sort/images/GitHub-Mark-32px.png')}></img>
+                    <a className='my-name' href='https://github.com/TerryHintz'>{"Leo Zhang"}</a>
+                    <img alt='github' className='github-icon' src={require('../sort/images/GitHub-Mark-32px.png')}></img>
+                </div>
             </div>
 		);
 	}
