adff222af9fe367e3b621d92bd2511eb04f81f7b
TerryHintz <cza82@sfu.ca>
2020-05-24 19:53:33 -0700

    spinning circles

--- src/home/About.js
+++ src/home/About.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './About.css';
+import { motion } from 'framer-motion';
 
 class About extends Component {
     render() {
@@ -8,6 +9,36 @@ class About extends Component {
                 <div className='about-container'>
                     <p className='about-title'>Hi, I'm Leo, an aspiring Software Development Engineer</p>
                     <img className='about-img' src={require('./images/bitmoji_smile.png')}/>
+                    <div style={{height: '0'}}>
+                        <motion.div
+                            style={{top: '-386px'}}
+                            className={'about-rotate'}
+                            animate={{rotate: 360}}
+                            transition={{loop: Infinity, duration: 8, ease: 'linear'}}
+                        >
+                            <img src={require('./images/green.png')}/>
+                        </motion.div>
+                    </div>
+                    <div style={{height: '0'}}>
+                        <motion.div
+                            style={{top: '-385px'}}
+                            className={'about-rotate'}
+                            animate={{rotate: 360}}
+                            transition={{loop: Infinity, duration: 6, ease: 'linear'}}
+                        >
+                            <img src={require('./images/purple.png')}/>
+                        </motion.div>
+                    </div>
+                    <div style={{height: '0'}}>
+                        <motion.div
+                            style={{top: '-360px'}}
+                            className={'about-rotate'}
+                            animate={{rotate: 360}}
+                            transition={{loop: Infinity, duration: 10, ease: 'linear'}}
+                        >
+                            <img src='https://github-atom-io-herokuapp-com.freetls.fastly.net/assets/index-portal-blue-50996d0762fb6d9ee13d6a52404dd01327fc542cc729777d532c9b633103c835.svg'/>
+                        </motion.div>
+                    </div>
                     <p className='about-subtitle'>Currently Studying Computer Engineering at SFU</p>
                 </div>
             </div>
