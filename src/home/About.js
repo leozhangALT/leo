dda40e0f8f6c613d89958a9ee39d3fbff0eb66b3
TerryHintz <cza82@sfu.ca>
2020-05-24 17:10:20 -0700

    about setup

---
+++ src/home/About.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './Home.css';
+
+class About extends Component {
+    render() {
+        return(
+            <div>about</div>
+        )
+    }
+}
+
+export default About;
--- src/home/Home.js
+++ src/home/Home.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
-import { Link } from 'react-router-dom';
 import './Home.css';
 
 import Projects from './Projects'
+import About from './About'
 
 const THEME = 'cornflowerblue'
 
@@ -11,6 +11,9 @@ class Home extends Component {
   render() {
     return (
       <div>
+        <About
+          theme = {THEME}
+        />
         <Projects
           theme = {THEME}
         />
